a390bf56960f52bc8753d80fb386f6dad01b10da
Shela Duong <sheladuong@gmail.com>
2020-06-07 10:58:37 -0700

    added in correct local storage toggles and line through toggles

--- todo.js
+++ todo.js
@@ -1,98 +1,85 @@
 const form = document.querySelector('form');
 const ul = document.querySelector('.todolist');
-const taskInput = document.querySelector('input[type = "text"]')
+const taskInput = document.querySelector('input[type = "text"]');
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let newElement = makeElement(taskInput.value);
-    ul.append(newElement);
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	let newElement = makeElement(taskInput.value);
+	ul.append(newElement);
 
-    let removeButton = document.createElement('Button');
-    removeButton.innerText = 'x';
-    removeButton.classList.add('rmv');
-    newElement.append(removeButton);
+	let removeButton = document.createElement('Button');
+	removeButton.innerText = 'x';
+	removeButton.classList.add('rmv');
+	newElement.append(removeButton);
 
+	let newLocalStorage = JSON.parse(localStorage.getItem('To Do: '));
+	if (!Array.isArray(newLocalStorage)) {
+		newLocalStorage = [];
+	}
+	newLocalStorage.push({
+		name: taskInput.value,
+		lineThrough: false
+	});
 
-    let newLocalStorage = JSON.parse(localStorage.getItem('To Do: '));
-    if (!Array.isArray(newLocalStorage)) {
-        newLocalStorage = [];
-    }
-    newLocalStorage.push({
-        name: taskInput.value,
-        lineThrough: false
-    });
+	localStorage.setItem('To Do: ', JSON.stringify(newLocalStorage));
 
-    localStorage.setItem('To Do: ', JSON.stringify(newLocalStorage))
-
-    taskInput.value = '';
-
+	taskInput.value = '';
 });
-
 
 function makeElement(value) {
-    let task = document.createElement('li');
-    task.innerText = value;
-    task.classList.add('taskChar')
-    return task;
+	let task = document.createElement('li');
+	task.innerText = value;
+	task.classList.add('taskChar');
+	return task;
 }
 
+ul.addEventListener('click', function(e) {
+	if (e.target.tagName === 'BUTTON') {
+		e.target.parentElement.remove();
 
-ul.addEventListener('click', function (e) {
+		let newLocalStorage = JSON.parse(localStorage.getItem('To Do: '));
+		for (let i = 0; i < newLocalStorage.length; i++) {
+			if (newLocalStorage[i].name + 'x' === e.target.parentElement.innerText) {
+				newLocalStorage.splice(i, 1);
+			}
+		}
 
-    if (e.target.tagName === 'BUTTON') {
-        e.target.parentElement.remove();
+		localStorage.setItem('To Do: ', JSON.stringify(newLocalStorage));
+	}
 
+	if (e.target.tagName === 'LI') {
+		e.target.style.textDecoration = e.target.style.textDecoration ? null : 'line-through';
 
-        let newLocalStorage = JSON.parse(localStorage.getItem('To Do: '));
-        for (let i = 0; i < newLocalStorage.length; i++) {
-            if (newLocalStorage[i].name + 'x' === e.target.parentElement.innerText) {
-                newLocalStorage.splice(i, 1);
-            }
-        }
+		let removeCross = JSON.parse(localStorage.getItem('To Do: '));
+		for (let i = 0; i < removeCross.length; i++) {
+			if (removeCross[i].name + 'x' === e.target.innerText) {
+				removeCross[i]['lineThrough'] = removeCross[i]['lineThrough'] ? false : true;
+			}
+		}
 
-        localStorage.setItem('To Do: ', JSON.stringify(newLocalStorage));
-
-    }
-
-
-    if (e.target.tagName === 'LI') {
-        e.target.style.textDecoration = 'line-through';
-
-        let removeCross = JSON.parse(localStorage.getItem('To Do: '));
-        for (let i = 0; i < removeCross.length; i++) {
-            if (removeCross[i].name + 'x' === e.target.innerText) {
-                removeCross[i]['lineThrough'] = true;
-            }
-        }
-
-        localStorage.setItem('To Do: ', JSON.stringify(removeCross));
-
-    }
+		localStorage.setItem('To Do: ', JSON.stringify(removeCross));
+	}
 });
 
+window.addEventListener('load', function(e) {
+	let newLocalStorage = [];
+	try {
+		newLocalStorage = JSON.parse(localStorage.getItem('To Do: '));
+	} catch (e) {
+		localStorage.setItem('To Do: ', JSON.stringify(newLocalStorage));
+	}
 
+	for (let toDo of newLocalStorage) {
+		let newElement = makeElement(toDo.name);
+		ul.append(newElement);
 
-window.addEventListener('load', function (e) {
+		let removeButton = document.createElement('Button');
+		removeButton.innerText = 'x';
+		removeButton.classList.add('rmv');
+		newElement.append(removeButton);
 
-    let newLocalStorage = [];
-    try {
-        newLocalStorage = JSON.parse(localStorage.getItem('To Do: '));
-    } catch (e) {
-        localStorage.setItem('To Do: ', JSON.stringify(newLocalStorage))
-    }
-
-    for (let toDo of newLocalStorage) {
-        let newElement = makeElement(toDo.name);
-        ul.append(newElement);
-
-        let removeButton = document.createElement('Button');
-        removeButton.innerText = 'x';
-        removeButton.classList.add('rmv');
-        newElement.append(removeButton);
-
-        if (toDo.lineThrough === true) {
-            newElement.style.textDecoration = 'line-through';
-        }
-    }
-
+		if (toDo.lineThrough === true) {
+			newElement.style.textDecoration = 'line-through';
+		}
+	}
 });
